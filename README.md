7063554995343b992fbf6cd1243a3483eaef0230
MwangiMurimi <mwangimurimi15@gmail.com>
2019-11-02 16:57:32 +0300

    make new repo

---
+++ README.md
@@ -0,0 +1,28 @@
+# Delani Studio Landing Page Clone
+This is a clone of Delani Studio landing page, made on Nov 1, 2019 by Mwangi Murimi.
+## Live link
+http://mwangimurimi.github.io/delani-clone
+## Set-up instructions
+* To use online, go to http://mwangimurimi.github.io/delani-clone in your browser
+* To use offline,
+
+```terminal
+git clone MwangiMurimi@github.com/delani-clone
+``` 
+then extract the file and open the index.html in your browser
+
+## Technologies used
+HTML, CSS, Javascript, Bootstrap, JQuery
+## Screenshots
+### Original
+![ Delani Studio](https://user-images.githubusercontent.com/56479833/68071884-36eb0280-fd90-11e9-9953-6a5605f2d67f.jpg)
+
+### Clone
+![screencapdelani](https://user-images.githubusercontent.com/56479833/68071858-fbe8cf00-fd8f-11e9-898a-897a92bc4f08.png)
+
+
+
+## Support and contact details
+you may reach me at mwangimurimi15@gmail.com
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+  $("#mouseclick").click(function() {
+    $("body").toggleClass("texture")
+  })
+  $(".des").click(function () {
+    $("#des-icon").toggle();
+    $("#des-text").toggle();
+  })
+  $(".dev").click(function () {
+    $("#dev-icon").toggle();
+    $("#dev-text").toggle();
+  })
+  $(".prod").click(function () {
+    $("#prod-icon").toggle();
+    $("#prod-text").toggle();
+  })
+  $("#grey-picture").hover(function () {
+    $("#grey").toggle();
+  })
+  $("#mighty-ontario").hover(function () {
+    $("#mighty").toggle();
+  })
+  $("#orange-thingy").hover(function () {
+    $("#orange").toggle();
+  })
+  $("#jim-carrey").hover(function () {
+    $("#jim").toggle();
+  })
+  $("#elena-blog").hover(function () {
+    $("#elena").toggle();
+  })
+  $("#button-button").hover(function () {
+    $("#button").toggle();
+  })
+  $("#burned").hover(function () {
+    $("#burn").toggle();
+  })
+  $("#giraffe-restaurant").hover(function () {
+    $("#giraffe").toggle();
+  });
+  $("#contactus textarea").keypress(function (e) {
+    if (e.which == 13) {
+      $('form').submit();
+    }
+  });
+  $("form").submit(function () {
+    alert("Thank you for your message.");
+  });
+});
